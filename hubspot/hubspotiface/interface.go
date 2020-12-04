4dc6777738e0e7b47ec072ee0a566afdc708810a
Bryan Elie <belie@teamexos.com>
2020-12-03 22:46:40 -0500

    XN-1259 update the interface to include the ReadContact function

--- hubspot/hubspotiface/interface.go
+++ hubspot/hubspotiface/interface.go
@@ -9,6 +9,7 @@ type HubSpotClient interface {
 	CreateAssociation(association *hubspot.AssociationInput, from string, to string) (*hubspot.AssociationResults, hubspot.AssociationErrorResponse)
 	CreateContact(contactInput *hubspot.ContactInput) (*hubspot.ContactOutput, hubspot.ErrorResponse)
 	UpdateContact(contactID string, contactInput *hubspot.ContactInput) (*hubspot.ContactOutput, hubspot.ErrorResponse)
+	ReadContact(email string, properties string) (*hubspot.ContactOutput, hubspot.ErrorResponse)
 }
 
 // make sure hubspot.Client type satisfies the HubSpotClient interface
